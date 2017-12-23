4614e1e5c99394c935099ff189afc2b79722a3b2
Pranav Dixit <prnvdixit@gmail.com>
2017-12-23 23:47:27 +0530

    Add pyautogui template file

---
+++ pyautogui_template.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+print pyautogui.confirm('This displays text and has an OK and Cancel button.')
+print pyautogui.alert('This displays some text with an OK button.')
+print pyautogui.prompt('This lets the user type in a string and press OK.')
