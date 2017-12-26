ab27fdb6cc1c5c949661929da7d62ab2348cb394
Pranav Dixit <prnvdixit@gmail.com>
2017-12-26 19:57:57 +0530

    Add keyboard over PyGame

--- tracking_colour.py
+++ tracking_colour.py
@@ -5,15 +5,18 @@ import cv2
 import pygame
 import sys
 import imutils
+import temp
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-b", "--buffer", type=int, default=32, help="max buffer size")
-global args
 args = vars(ap.parse_args())
 
 crop_img_offset = 15
 
 pts = deque(maxlen=args["buffer"])
+
+keyboard_img = pygame.image.load("keyboard.png")
+
 
 #
 # gameDisplay = pygame.display.set_mode((640, 480))
@@ -23,8 +26,8 @@ pts = deque(maxlen=args["buffer"])
 # display_width = desktopWidth
 # display_height = desktopHeight
 
-def track():
 
+def track():
     object_set_to_be_detected = 0
 
     vector = np.zeros((2, 1), dtype=np.int)
@@ -37,15 +40,18 @@ def track():
     direction = ""
 
     pygame.init()
-    gameDisplay = pygame.display.set_mode(frame.shape[:2])
+    game_display = pygame.display.set_mode(frame.shape[:2])
     pygame.display.set_caption('hsv_approximation')
     pygame.mouse.set_visible(False)
 
     while grabbed:
         (grabbed, frame) = camera.read()
 
+        if temp.mode == "virtual_keyboard":
+            pygame.mouse.set_visible(True)
+
         # cv2.imwrite("flag.jpg", frame)
-	
+
         # frame_image = pygame.image.load("flag.jpg")
         # gameDisplay.blit(frame_image, (0, 0))
 
@@ -58,11 +64,10 @@ def track():
                 # print "Mouse", pos
 
                 img = frame
-                # TODO : Compare the results with frame.copy()
-
                 # print path, img
 
-                img_crop = img[y_co - crop_img_offset:y_co + crop_img_offset, x_co - crop_img_offset:x_co + crop_img_offset]
+                img_crop = img[y_co - crop_img_offset:y_co + crop_img_offset,
+                           x_co - crop_img_offset:x_co + crop_img_offset]
                 img = cv2.cvtColor(img_crop, cv2.COLOR_BGR2HSV)
 
                 length = len(img)
@@ -85,17 +90,28 @@ def track():
                 object_set_to_be_detected = 1
 
                 img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-                # TODO : This won't be required if frame.copy() gave good results
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
-                    # print colourUpper, colourLower
+                    # print colour_upper, colour_lower
                     sys.exit(0)
 
                 elif event.key == pygame.K_s:
                     # print "Key pressed"
                     pts.clear()
                     object_set_to_be_detected = 1 - object_set_to_be_detected
+
+                elif event.key == pygame.K_k:
+                    temp.mode = "virtual_keyboard"
+
+                elif event.key == pygame.K_p:
+                    temp.mode = "presentation"
+
+                elif event.key == pygame.K_m:
+                    temp.mode = "media"
+
+                elif event.key == pygame.K_r:
+                    temp.mode = "reading"
 
             else:
                 pass
@@ -107,25 +123,34 @@ def track():
         # print "2", frame.shape
         # print frame.shape
         # pygame.pixelcopy.array_to_surface(gameDisplay, frame)
-        pygame.surfarray.blit_array(gameDisplay, frame)
+        pygame.surfarray.blit_array(game_display, frame)
         # print "3", frame.shape
         frame = np.transpose(frame, (1, 0, 2))
         # print "4", frame.shape
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-        position_mouse = pygame.mouse.get_pos()
-        pygame.draw.rect(gameDisplay, (0, 0, 0), (position_mouse[0] - crop_img_offset, position_mouse[1] - crop_img_offset, 2 * crop_img_offset, 2 * crop_img_offset), 2)
+        if temp.mode == "virtual_keyboard":
+            game_display.blit(keyboard_img, (0, 0))
+
+        if temp.mode != "virtual_keyboard":
+            position_mouse = pygame.mouse.get_pos()
+            pygame.draw.rect(
+                game_display, (0, 0, 0), (position_mouse[0] - crop_img_offset,
+                    position_mouse[1] - crop_img_offset,
+                    2 * crop_img_offset,
+                    2 * crop_img_offset), 2
+            )
 
         if object_set_to_be_detected:
-            colourLower = np.array([result[0][0], result[0][1], result[0][2]], dtype="uint8")
-            colourUpper = np.array([result[1][0], result[1][1], result[1][2]], dtype="uint8")
+            colour_lower = np.array([result[0][0], result[0][1], result[0][2]], dtype="uint8")
+            colour_upper = np.array([result[1][0], result[1][1], result[1][2]], dtype="uint8")
 
             frame = imutils.resize(frame, width=640)
             blur_frame = cv2.GaussianBlur(frame, (11, 11), 0)
             hsv = cv2.cvtColor(blur_frame, cv2.COLOR_BGR2HSV)
 
-            # print colourLower, colourUpper
-            mask = cv2.inRange(hsv, colourLower, colourUpper)
+            # print colour_lower, colour_upper
+            mask = cv2.inRange(hsv, colour_lower, colour_upper)
             mask = cv2.erode(mask, None, iterations=2)
             mask = cv2.dilate(mask, None, iterations=2)
 
@@ -140,7 +165,7 @@ def track():
                 x, y, w, h = cv2.boundingRect(c)
 
                 center = (x + w / 2, y + h / 2)
-                pygame.draw.rect(gameDisplay, (0, 255, 0), (x, y, w, h), 2)
+                pygame.draw.rect(game_display, (0, 255, 0), (x, y, w, h), 2)
                 # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 pts.appendleft(center)
 
@@ -156,22 +181,22 @@ def track():
                     continue
 
                 # if len(pts) > 8 and i == 1 and pts[-8] is not None:
-                #     dX = pts[-8][0] - pts[i][0]
-                #     dY = pts[-8][1] - pts[i][1]
-                #     (dirX, dirY) = ("", "")
+                #     d_x = pts[-8][0] - pts[i][0]
+                #     d_y = pts[-8][1] - pts[i][1]
+                #     (dir_x, dir_y) = ("", "")
                 #
                 #
-                #     if np.abs(dX) > 10:
-                #         dirX = "East" if np.sign(dX) == 1 else "West"
-                #     if np.abs(dY) > 10:
-                #         dirY = "North" if np.sign(dY) == 1 else "South"
+                #     if np.abs(d_x) > 10:
+                #         dir_x = "East" if np.sign(d_x) == 1 else "West"
+                #     if np.abs(d_y) > 10:
+                #         dir_y = "North" if np.sign(d_y) == 1 else "South"
                 #
-                #     if dirX == "":
-                #         direction = dirY
-                #     elif dirY == "":
-                #         direction = dirX
+                #     if dir_x == "":
+                #         direction = dir_y
+                #     elif dir_y == "":
+                #         direction = dir_x
                 #     else:
-                #         direction = dirX if abs(dY)/abs(dX) >= 1 else dirY
+                #         direction = dir_x if abs(d_y)/abs(d_x) >= 1 else dir_y
                 #
                 #     font = pygame.font.SysFont("timesnewroman", size=25, bold="False", italic="True")
                 #     screen_text = font.render(direction, True, (0, 0, 0))
@@ -181,43 +206,44 @@ def track():
 
                 thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 3)
                 # cv2.line(frame, pts[i - 1], pts[i], (0, 255, 0), thickness)
-                pygame.draw.line(gameDisplay, (0, 255, 0), pts[i - 1], pts[i], thickness)
+                pygame.draw.line(game_display, (0, 255, 0), pts[i - 1], pts[i], thickness)
 
             if np.linalg.norm(vector) > 200:
                 # print "Direction", vector
                 # print np.linalg.norm(vector)
-                dX = vector[0]
-                dY = vector[1]
+                d_x = vector[0]
+                d_y = vector[1]
 
-                (dirX, dirY) = ("", "")
+                (dir_x, dir_y) = ("", "")
 
-                if np.abs(dX) > 150:
-                    dirX = "West" if np.sign(dX) == 1 else "East"
-                if np.abs(dY) > 150:
-                    dirY = "South" if np.sign(dY) == 1 else "North"
+                if np.abs(d_x) > 150:
+                    dir_x = "West" if np.sign(d_x) == 1 else "East"
+                if np.abs(d_y) > 150:
+                    dir_y = "South" if np.sign(d_y) == 1 else "North"
 
-                if dirX == "":
-                    direction = dirY
-                elif dirY == "":
-                    direction = dirX
+                if dir_x == "":
+                    direction = dir_y
+                elif dir_y == "":
+                    direction = dir_x
                 else:
-                    direction = dirX if abs(dY)/abs(dX) >= 1 else dirY
+                    direction = dir_x if abs(d_y) / abs(d_x) >= 1 else dir_y
 
                 vector = np.zeros((2, 1), dtype=np.int)
                 # print direction
 
             font = pygame.font.SysFont("timesnewroman", size=25, bold="False", italic="True")
-            screen_text = font.render(direction, True, (0, 0, 0))
+            screen_text = font.render(direction, True, (255, 255, 255))
             text_position = screen_text.get_rect()
-            text_position.center = (100, 100)
-            gameDisplay.blit(screen_text, text_position)
+            text_position.center = (350, 450)
+            game_display.blit(screen_text, text_position)
 
         pygame.display.update()
 
         key = cv2.waitKey(1) & 0xFF
 
         if key == ord('q'):
-            # print colourUpper, colourLower
+            # print colour_upper, colour_lower
             break
 
 track()
+
